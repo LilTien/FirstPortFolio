a133599e1e883c4f2a5066567b87b5d4df1fcc91
Alif <64727259+LilTien@users.noreply.github.com>
2024-07-29 00:34:08 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        widesttt:'.5rem',
+      },
+      margin: {
+        mag40: '160px',
+      },
+      padding: {
+        pad40: '160px',
+      },
+    },
+  },
+  plugins: [],
+}
+
